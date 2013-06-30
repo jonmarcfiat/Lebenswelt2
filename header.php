1526ec336f7005d6f911cdf7e56199cffacedca5
andrewhaines07 <andrew.m.haines@gmail.com>
2013-06-30 05:54:18 -0400

    Removed FB script

--- header.php
+++ header.php
@@ -51,16 +51,6 @@
 
 </head><!-- End Head -->
 
-<!-- Facebook script -->
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=304418233011542";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-
 
 <body <?php body_class(); ?>>
 
