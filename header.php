5b4bae7ebe7aa9f14bbdd8a378fcef5747ae8b02
andrewhaines07 <andrew.m.haines@gmail.com>
2013-07-05 12:35:29 -0400

    Update header.php

--- header.php
+++ header.php
@@ -51,6 +51,16 @@
 
 </head><!-- End Head -->
 
+<!-- Facebook script -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=304418233011542";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
 
 <body <?php body_class(); ?>>
 
@@ -66,17 +76,14 @@
 	<!-- ####### -- Banner -- ####### -->
 	<?php get_template_part ('banner');?>
 
-
 	<!-- ####### -- TitleBar -- ####### -->
 	<?php get_template_part ('titlebar');?>
 
-
 	<!-- ####### -- Navigation -- ####### -->
 	<?php get_template_part ('navigation');?>
-	
+
 	<!-- ####### -- Main Slider -- ####### -->
 	<?php get_template_part ('main-slider');?>
-
 
 
 	<!-- MAIN CONTENT -->
